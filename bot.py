3fe46c3fef4eee0e209a2bf6b186991d0b45a90b
Ryo <54506591+Ryo1018@users.noreply.github.com>
2022-06-02 01:24:29 +0900

    auto disconnect after played FIFA anthem

--- bot.py
+++ bot.py
@@ -1,29 +1,21 @@
 # pip install pynacl, libffi-dev, ffmpeg
 
-
-# インストールした discord.py を読み込む
+import time
 import discord
 
-# 自分のBotのアクセストークンに置き換えてください
-TOKEN = 'OTQ0NjIxOTA4NjczMDU2Nzg4.GsFZ0a.eDg40c3vBlafKSkk4t0Eg4zo_TNAWAI9Gwr1eU'
+TOKEN = ''
 
-# 接続に必要なオブジェクトを生成
 client = discord.Client()
 
-# 起動時に動作する処理
 @client.event
 async def on_ready():
     print('ログインしました')
 
-# メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    # if message.content == '$neko':
-    #     await message.channel.send('にゃーん')
 
     if message.content == '$FIFA':
         '''
@@ -32,23 +24,20 @@ async def on_message(message):
         - create new voicechannel
         - The user in new voicechannel, after return before voicechannel
         '''
-        
-        # voice = await client.join_voice_channel(client.get_channel("PASTE ID"))
-        # player = voice.create_ffmpeg_player('fifa.mp3')
-        # player.start()
 
         if message.author.voice is None:
             await message.channel.send('あなたは会場に入っていません。')
             return
-        
-        await message.author.voice.channel.connect()
-        await message.channel.send('開場')
-        
-        await message.guild.voice_client.play(discord.FFmpegPCMAudio('FIFA.mp3'))
+        else:
+            await message.author.voice.channel.connect()
+            await message.channel.send('開場')
+            
+            message.guild.voice_client.play(discord.FFmpegPCMAudio('FIFA.mp3'))
 
-        await message.guild.voice_client.disconnect()
-        await message.channel.send('閉場')
+            time.sleep(20)
 
+            await message.guild.voice_client.disconnect()
+            await message.channel.send('閉場')
 
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
