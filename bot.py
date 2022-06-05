2a849975985cf2e278e2af703294cb2d00d15335
Ryo <54506591+Ryo1018@users.noreply.github.com>
2022-06-06 00:50:40 +0900

    サロメコマンド　テスト

--- bot.py
+++ bot.py
@@ -2,8 +2,10 @@
 
 import time
 import discord
+import random
+from decimal import Decimal, ROUND_HALF_UP
 
-TOKEN = ''
+TOKEN = 'OTQ0NjIxOTA4NjczMDU2Nzg4.GcilVp.K3S_h8ITgszm0INySXO1-Zb5d7q6OU1W-8amMk'
 
 client = discord.Client()
 
@@ -26,7 +28,7 @@ async def on_message(message):
         '''
 
         if message.author.voice is None:
-            await message.channel.send('あなたは会場に入っていません。')
+            await message.channel.send(f'{message.author.mention} あなたは会場に入っていません。帰れ！バケモノ')
             return
         else:
             await message.author.voice.channel.connect()
@@ -39,5 +41,38 @@ async def on_message(message):
             await message.guild.voice_client.disconnect()
             await message.channel.send('閉場')
 
-# Botの起動とDiscordサーバーへの接続
+    if message.content == '$SLM':
+        if message.author.voice is None:
+            await message.channel.send(f'{message.author.mention} 虫けらはお帰りくださいまし～！')
+            return
+        else:
+            await message.author.voice.channel.connect()
+            message.guild.voice_client.play(discord.FFmpegPCMAudio('salome.mp3'))
+            time.sleep(15)
+            await message.guild.voice_client.disconnect()
+
+    if message.content == '$OMIKUJI':
+        if message.author.voice is None:
+            await message.channel.send(f'{message.author.mention} 虫けらはお帰りくださいまし～！')
+            return
+        else:
+            await message.author.voice.channel.connect()
+            rnd = random.uniform(0, 3)
+            rundrnd = Decimal(rnd*100).quantize(Decimal('0.1'), rounding=ROUND_HALF_UP)
+            await message.channel.send(f'{message.author.mention} {rundrnd}%ですわよ～！')
+            message.guild.voice_client.play(
+                discord.PCMVolumeTransformer(discord.FFmpegPCMAudio('salome.mp3'), volume=rundrnd)
+                )
+            time.sleep(15)
+            await message.guild.voice_client.disconnect()
+    
+    if message.content == '$TEXT':
+        text = input()
+        # await message.delete()
+        await message.channel.send(text)
+
+    if message.content == '$KILL':
+        await message.channel.send('⏰')
+        await client.close()
+
 client.run(TOKEN)
